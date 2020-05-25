17b3c71729755211fa8be36ac886ae5e872308b4
leo-brown244 <lionelbrown244@gmail.com>
2020-05-25 03:29:16 +0300

    initial commit

---
+++ MVC/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MVC.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /<controller>/
+        public string Index()
+        {
+            return "hello world";
+        }
+
+        public string Welcome(string name, string ID)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+
+    }
+}
